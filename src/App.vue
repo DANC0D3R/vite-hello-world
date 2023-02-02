695b86e2aef5b8cdc0ef273504b2aab6e65bbe73
Daniele Minieri <danieleminieri99@gmail.com>
2023-02-02 10:45:13 +0100

    Bonus completato

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import AppHero from './components/AppHero.vue';
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Hello World con Vite" />
+  <AppHero/>
 </template>
 
 <style scoped>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,44 @@
+<script>
+    export default {
+        data() {
+            return {
+                img: 'https://unsplash.it/1000/500',
+            }
+        }
+    }
+</script>
+
+<template>
+    <div id="jumbo">
+        <img :src="img" alt="jumbotron">
+    </div>
+</template>
+
+<style scoped>
+    #jumbo {
+        position: relative;
+
+        width: 100%;
+        height: 500px;
+
+        overflow: hidden;
+    }
+    #jumbo img {
+        width: 100%;
+        height: 100%;
+
+        object-fit: cover;
+    }
+    #jumbo h2 {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        margin: 0;
+
+        font-size: 4rem;
+        text-transform: uppercase;
+        text-shadow: 0px 0px 10px #000000;
+    }
+</style>
